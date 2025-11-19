db472dd5775a3832dcd7a924214b0f754f55ef63
ItsForJax <dadelos.ddls@gmail.com>
2025-11-19 14:49:30 +0000

    fix: fixed line 9 missing qoute

--- sync.sh
+++ sync.sh
@@ -6,4 +6,4 @@
 # Sync with rsync (faster, shows progress, only uploads changed files)
 rsync -avz --progress -e "ssh -p 65002" public_html/ $SSH_USER@$SSH_IP:~/websites/unwPIhhdu/public_html/
 
-echo "Folder synced successfully!
+echo "Folder synced successfully!"
